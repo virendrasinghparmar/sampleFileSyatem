08ecb1ca1526afc805bea29d43abb13021adee8d
Virendra Singh Parmar <98700748+virendrasinghparmar@users.noreply.github.com>
2025-11-09 20:21:14 +0530

    Update settings.py

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-test-key'
 DEBUG = True
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
